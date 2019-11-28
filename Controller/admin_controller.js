feb429b96ce24bcdf42a08cdb9c8f6caf74fb67c
sabrinayyu <54804569+sabrinayyu@users.noreply.github.com>
2019-11-28 15:00:30 -0500

    screen 14&16 added, screen15 improved
    screen15: add default value to drop down list & update mysql query by considering unassigned managers

--- Controller/admin_controller.js
+++ Controller/admin_controller.js
@@ -182,8 +182,18 @@ exports.create_theater_get = async function (req, res, next) {
         res.render('index', { title: 'Hello',
             errors: [{msg: "You are not an administrator, you have no right to view the page!"}], sess: req.session});
     } else {
-        var sql1 = "Select Name from company";
-        var sql2 = "Select UserName from manager where UserName not in (select managerUsername from theater)";
+        //set the first value in the dropdown list
+        var target_companyName = req.query.target_companyName;
+        // console.log('target_companyName is');
+        // console.log(target_companyName);
+        if (typeof(target_companyName) == "undefined") {
+            target_companyName = "4400 Theater Company";
+        }
+        console.log(target_companyName);
+        //success!
+
+        var sql1 = "Select Name from company order by Name = ? desc";
+        var sql2 = "SELECT UserName, Works_In FROM manager WHERE UserName NOT IN (SELECT ManagerUsername FROM theater) AND Works_In = ? ";
         const companys = await dbQuery(sql1);
         const managerNames = await dbQuery(sql2);
         res.render('admin_create_theater', {title: "Create Theater", companys: companys, managerNames: managerNames, errors: []});
@@ -279,6 +289,227 @@ exports.create_movie_post = [
         }
     }
 ];
+
+// screen 14
+let companys1 = [];
+exports.manage_company_get = [
+    //(ignore Validate fields.in this case)
+    // Sanitize fields (using wildcard).
+    sanitizeQuery('*').escape(),
+(req, res, next) => {
+        // Extract the validation errors from a request.
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            res.render('admin_filter_company', {title: 'Manage Company',
+                data: [], errors: errors.array()});
+        }
+        else if (isEmptyObj(req.query)) {
+            var sql = "Select Name from company";
+
+            db.query(sql, [], (error, results1, fields) => {
+                if (error) {
+                    return console.error(error.message);
+                }
+                console.log("successfully retrieved the company list!");
+                //console.log(results);
+                companys1 = companys1.concat(results1);
+                //console.log(companys1);
+            });
+
+
+            var testSql = "call admin_filter_company('ALL', 1, 100, 1, 100, 1, 100, '', '')";
+            //
+            
+            db.query(testSql, [companyName, mincityCovered, maxcityCovered, mintheaters, maxtheaters, minemployees, maxemployees, sortBy, sortDirection], (error, results, fields) => {
+                if (error) {
+                    return console.error(error.message);
+                }
+            });
+
+            // poll from newly created table and send it to our view
+            var pollSql = "Select * From AdFilterCom";
+            db.query(pollSql, [], (error, results2, fields) => {
+                if (error) {
+                    return console.error(error.message);
+                }
+
+            //console.log(results2);
+            //success!
+
+            res.render('admin_filter_company', {title: 'Manage Company', companys: companys1, data: results2, errors: []})
+            });
+
+
+
+        } else {
+
+            var companyName = req.query.companyName;
+
+            var mincityCovered = parseInt(req.query.mincityCovered);
+            if (isNaN(mincityCovered)) {
+                mincityCovered = null;
+            }
+            var maxcityCovered = parseInt(req.query.maxcityCovered);
+            if (isNaN(maxcityCovered)) {
+                maxcityCovered = null;
+            }
+            var mintheaters = parseInt(req.query.mintheaters);
+            if (isNaN(mintheaters)) {
+                mintheaters = null;
+            }
+            var maxtheaters = parseInt(req.query.maxtheaters);
+            if (isNaN(maxtheaters)) {
+                maxtheaters = null;
+            }
+            var minemployees = parseInt(req.query.minemployees);
+            if (isNaN(minemployees)) {
+                minemployees = null;
+            }
+            var maxemployees = parseInt(req.query.maxemployees);
+            if (isNaN(maxemployees)) {
+                maxemployees = null;
+            }
+
+
+            var sortBy = '';
+            var sortDirection = '';
+
+            // for sort use
+            req.session.screen14Combination = [companyName, mincityCovered, maxcityCovered, mintheaters, maxtheaters, minemployees, maxemployees, sortBy, sortDirection];
+            //console.log(req.session.screen14Combination);
+
+            // call procedure first, create the table that has information
+            var testSql = "call admin_filter_company(?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        
+            db.query(testSql, [companyName, mincityCovered, maxcityCovered, mintheaters, maxtheaters, minemployees, maxemployees, sortBy, sortDirection], (error, results, fields) => {
+                if (error) {
+                    return console.error(error.message);
+                }
+            });
+
+            // poll from newly created table and send it to our view
+            var pollSql = "Select * From AdFilterCom";
+            db.query(pollSql, [], (error, results, fields) => {
+                if (error) {
+                    return console.error(error.message);
+                }
+            //console.log(results);
+
+            res.render('admin_filter_company', {title: 'Manage Company', companys: companys1,
+                data: results, errors: []});
+            });
+        }
+    }
+]
+
+let directC =  1;
+exports.company_detail_sort = function(req, res, next) {
+
+    var sortBy = req.query.sortBy;
+    var sortDirection;
+    directC *= -1;
+            
+    if(!sortBy) {
+        sortBy = '';
+    }
+    console.log(directC);
+    if(directC == -1) {
+        sortDirection = 'DESC';
+    } else {
+        sortDirection = 'ASC';
+    }
+    console.log(sortDirection);
+
+    var combination = req.session.screen14Combination;
+    console.log(combination);
+    if (typeof(combination) == "undefined") {
+        combination = ['ALL', 1, 100, 1, 100, 1, 100, '', ''];
+        ;
+    } 
+    combination[7] = sortBy;
+    combination[8] = sortDirection;
+
+    var sql = "Select Name from company";
+
+    db.query(sql, [], (error, results1, fields) => {
+        if (error) {
+            return console.error(error.message);
+        }
+        console.log("successfully retrieved the company list!");
+        //console.log(results);
+       // console.log(results.length);
+        companys1 = companys1.concat(results1);
+        //console.log(companys1);
+    });
+
+
+    // call procedure first, create the table that has information
+    var testSql = "call admin_filter_company(?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    db.query(testSql, combination, (error, results, fields) => {
+        if (error) {
+            return console.error(error.message);
+        }
+    });
+
+    // poll from newly created table and send it to our view
+    var pollSql = "Select * From AdFilterCom";
+    db.query(pollSql, [], (error, results, fields) => {
+        if (error) {
+            return console.error(error.message);
+        }
+
+    console.log(results);
+    res.render('admin_filter_company', {title: 'Manage Company', companys: companys1,
+        data: results, errors: []});
+    });
+
+}
+
+
+//screen 16
+exports.comDetail_get =  async (req, res, next) => {
+
+        // Extract the validation errors from a request.
+        const errors = validationResult(req);
+
+        // if have logic errors
+        if (!errors.isEmpty()) {
+            res.render('company_detail', {title: "Wrong info typed!", data:[], errors:[]});
+        } else {
+            let target_companyName = req.query.target_companyName;
+            // console.log(target_companyName);
+
+            if (typeof(target_companyName) == "undefined") {
+                target_companyName = "4400 Theater Company";
+            }
+            console.log(target_companyName);
+
+            const sql1 = "call admin_view_comDetail_emp(?)";
+            db.query(sql1, [target_companyName], (error, results, fields) => {
+                if (error) {
+                    return console.error(error.message);
+                }
+            });
+            const pollSql1 = "SELECT * FROM team28.adcomdetailemp";
+            let comDetailEmp = await dbQuery(pollSql1, [target_companyName]);
+
+            const sql2 = "call admin_view_comDetail_th(?)";
+            db.query(sql2, [target_companyName], (error, results, fields) => {
+                if (error) {
+                    return console.error(error.message);
+                }
+            });
+            const pollSql2 = "SELECT * FROM team28.adcomdetailth;";
+            let comDetailTh = await dbQuery(pollSql2, [target_companyName]);
+            
+            // console.log(comDetailEmp);
+            // console.log(comDetailTh);
+            res.render('company_detail', {title: "Company Detail", cop: target_companyName, dataE: comDetailEmp, dataT: comDetailTh, errors:[]});
+
+            
+        }
+    }
 
 // Some helper functions:
 
